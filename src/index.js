45d405d4d09c9e2e2e9966dacf1a73e1f607ed81
Israel Yakovson <israely@ad.checkpoint.com>
2018-09-20 18:26:31 +0300

    Linting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 import numeral from 'numeral';
 
+/* eslint-disable no-console */
+
 const coursValue = numeral(1000).format('$0,0.00');
-debugger;
+//debugger;
 console.log(`I would pay ${coursValue} for this course!`);
